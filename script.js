8a730cf06a7add50177f56f2768ec92117aac8f1
MaxiCubas <151659599+MaxiCubas@users.noreply.github.com>
2023-12-02 21:23:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -41,87 +41,86 @@ document.addEventListener("DOMContentLoaded", () => {
       
       tarjetas = tarjetas.concat(tarjetas); //duplicamos los elementos del array
       const grid = document.getElementById("grid");
-var cartasSeleccionadas = []; //va a ser un array de los índices de cada tarjeta seleccionada
-var contadorGanados = 0;
-const resultado = document.getElementById("resultado"); //para mostrar los resultados a medida se encuentran los pares
-//este es el algoritmo de mezcla Fisher-Yates
+        var cartasSeleccionadas = []; //va a ser un array de los índices de cada tarjeta seleccionada
+        var contadorGanados = 0;
+        const resultado = document.getElementById("resultado"); 
 
-function revolverTarjetas(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];    //para el intercambio se utiliza una variable auxiliar
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
-  }
-  tarjetas = revolverTarjetas(tarjetas);
-//armamos el tablero de forma dinamica
+        function revolverTarjetas(array) {
+             for (let i = array.length - 1; i > 0; i--) {
+             const j = Math.floor(Math.random() * (i + 1));
+                const temp = array[i];    //para el intercambio se utiliza una variable auxiliar
+                array[i] = array[j];
+                 array[j] = temp;
+                    }
+                return array;
+                }
+                tarjetas = revolverTarjetas(tarjetas);
 
-    function armarTablero() {
-    for (let i = 0; i < tarjetas.length; i++) {
+
+        function armarTablero() {
+             for (let i = 0; i < tarjetas.length; i++) {
      
-        const tarjeta = document.createElement("img");
-//seteamos la imagen de patron por defecto
-tarjeta.setAttribute(
-  "src",
-  "images/—Pngtree—sweet avocado seamless pattern kawaii_990338.png"
-);
-tarjeta.setAttribute("data-index", i);
-//esto es para agregar las clases al objeto "tarjeta"
-tarjeta.classList.add("imagen");
-tarjeta.setAttribute("name", tarjetas[i].nombre);
-tarjeta.addEventListener("click", voltearTarjeta);
-// agregamos el elemento creado a la grilla
-grid.appendChild(tarjeta);
+            const tarjeta = document.createElement("img");
 
-  }
+                tarjeta.setAttribute(
+            "src",
+            "images/—Pngtree—sweet avocado seamless pattern kawaii_990338.png"
+                    );
+                tarjeta.setAttribute("data-index", i);
+                //esto es para agregar las clases al objeto "tarjeta"
+                tarjeta.classList.add("imagen");
+                tarjeta.setAttribute("name", tarjetas[i].nombre);
+                tarjeta.addEventListener("click", voltearTarjeta);
+                // agregamos el elemento creado a la grilla
+                grid.appendChild(tarjeta);
 
-  function voltearTarjeta() {
-    //obtenemos el índice del array de perros
-let index = this.getAttribute("data-index");
-cartasSeleccionadas.push(index);
-this.setAttribute("src", tarjetas[index].imagen);
-if (cartasSeleccionadas.length === 2) {
-    setTimeout(validarTarjetasSeleccionadas, 400);
-  }
+                 }
+
+        function voltearTarjeta() {
+             //obtenemos el índice del array de perros
+    let index = this.getAttribute("data-index");
+    cartasSeleccionadas.push(index);
+    this.setAttribute("src", tarjetas[index].imagen);
+        if (cartasSeleccionadas.length === 2) {
+        setTimeout(validarTarjetasSeleccionadas, 400);
+                 }
   
-  function validarTarjetasSeleccionadas() {
-    const listaTarjetas = document.querySelectorAll("img");
-    const primeraTarjetaSeleccionadaIndex = cartasSeleccionadas[0];
-    const segundaTarjetaSeleccionadaIndex = cartasSeleccionadas[1];
+        function validarTarjetasSeleccionadas() {
+            const listaTarjetas = document.querySelectorAll("img");
+            const primeraTarjetaSeleccionadaIndex = cartasSeleccionadas[0];
+            const segundaTarjetaSeleccionadaIndex = cartasSeleccionadas[1];
     
-    if (primeraTarjetaSeleccionadaIndex == segundaTarjetaSeleccionadaIndex) {
-        alert("¡Es la misma tarjeta!");
-        //ponemos de vuelva a ambas la imagen del patron por defecto
-        listaTarjetas[primeraTarjetaSeleccionadaIndex].setAttribute(
-          "src",
-          "images/—Pngtree—sweet avocado seamless pattern kawaii_990338.png"
-        );
-        listaTarjetas[segundaTarjetaSeleccionadaIndex].setAttribute(
-          "src",
-          "images/—Pngtree—sweet avocado seamless pattern kawaii_990338.png"
-        );
+        if (primeraTarjetaSeleccionadaIndex == segundaTarjetaSeleccionadaIndex) {
+            alert("¡Es la misma tarjeta!");
+             //ponemos de vuelva a ambas la imagen del patron por defecto
+            listaTarjetas[primeraTarjetaSeleccionadaIndex].setAttribute(
+              "src",
+              "images/—Pngtree—sweet avocado seamless pattern kawaii_990338.png"
+            );
+            listaTarjetas[segundaTarjetaSeleccionadaIndex].setAttribute(
+             "src",
+             "images/—Pngtree—sweet avocado seamless pattern kawaii_990338.png"
+            );
       }
     
       else if (listaTarjetas[primeraTarjetaSeleccionadaIndex].name === listaTarjetas[segundaTarjetaSeleccionadaIndex].name) {
-        //el atributo "name" guarda el nombre de la raza, si son iguales entonces es correcto
-        alert("¡Correcto!");
+            //el atributo "name" guarda el nombre de la raza, si son iguales entonces es correcto
+            alert("¡Correcto!");
         //cambiar la imagen por la del patron de finalizacion
         listaTarjetas[primeraTarjetaSeleccionadaIndex].setAttribute(
-          "src",
-          "images/—Pngtree—sweet avocado seamless pattern kawaii_990338 copia.png"
-        );
-        listaTarjetas[segundaTarjetaSeleccionadaIndex].setAttribute(
-          "src",
-          "images/—Pngtree—sweet avocado seamless pattern kawaii_990338 copia.png"
-        );
-        //evitamos que se pueda volver a hacer click en las mismas
-        listaTarjetas[primeraTarjetaSeleccionadaIndex].removeEventListener("click", voltearTarjeta);
-        listaTarjetas[segundaTarjetaSeleccionadaIndex].removeEventListener("click", voltearTarjeta);
-        contadorGanados = contadorGanados + 2; //sumamos 2 porque fueron 2 las cartas correctas
-      }
-      else {
+              "src",
+             "images/—Pngtree—sweet avocado seamless pattern kawaii_990338 copia.png"
+            );
+            listaTarjetas[segundaTarjetaSeleccionadaIndex].setAttribute(
+             "src",
+              "images/—Pngtree—sweet avocado seamless pattern kawaii_990338 copia.png"
+         );
+            //evitamos que se pueda volver a hacer click en las mismas
+            listaTarjetas[primeraTarjetaSeleccionadaIndex].removeEventListener("click", voltearTarjeta);
+            listaTarjetas[segundaTarjetaSeleccionadaIndex].removeEventListener("click", voltearTarjeta);
+         contadorGanados = contadorGanados + 2; //sumamos 2 porque fueron 2 las cartas correctas
+            }
+        else {
         listaTarjetas[primeraTarjetaSeleccionadaIndex].setAttribute(
           "src",
           "images/—Pngtree—sweet avocado seamless pattern kawaii_990338.png"
@@ -131,22 +130,18 @@ if (cartasSeleccionadas.length === 2) {
           "images/—Pngtree—sweet avocado seamless pattern kawaii_990338.png"
         );
       }
-      cartasSeleccionadas = [];
-      if (contadorGanados === tarjetas.length) {
-        resultado.textContent = "¡Felicidades! ¡Los encontraste a todos!";
-      } else {
-        resultado.textContent = (contadorGanados / 2) + " razas encontradas"; //hay 2n pares, por ende hay n elementos distintos
-      }
-  }
+      
+        cartasSeleccionadas = [];
+        if (contadorGanados === tarjetas.length) {
+          resultado.textContent = "¡Felicidades! ¡Los encontraste a todos!";
+        } else {
+          resultado.textContent = (contadorGanados / 2) + " parejas encontradas"; 
+        }
+    }
+    
+  } 
   
-} 
-
-  //creamos un elemento de tipo <img />
-
-//cada 2 tarjetas se hacen las validaciones
-
-
-}
+  }
 armarTablero();
-
-});
+  
+  });
